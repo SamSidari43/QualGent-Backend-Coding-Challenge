789bad8bb1ee4130c1f01eefa981c0c567d5c28f
SamSidari43 <samuelsidari@gmail.com>
2025-07-14 09:44:15 -0400

    Update JobController.java

--- job-server/src/main/java/com/qualgent/jobserver/controller/JobController.java
+++ job-server/src/main/java/com/qualgent/jobserver/controller/JobController.java
@@ -1,17 +1,28 @@
 package com.qualgent.jobserver.controller;
+
 import com.qualgent.jobserver.model.Job;
-inport com.qualgent.jobserver.service.JobService:
-import org.springframework.web, bind annotation.*=
+import com.qualgent.jobserver.service.JobService:
+import org.springframework.web, bind annotation.*;
+
 import java.util.UUID;
-Les I
+
 @RestController
 @RequestMapping ("/jobs")
-Mes y publio class Jobcontroller (
-private final Jooservice jobservice:
-public JobController(Jobservice JobService i
-this jobservice - jobservice:
-@PostMapping
-public Dob subnitJob(@RequestBody Joh job) {
-job.setId(UUID. randomUUID().tostringO);
-neturn jobService.submitlob(job);
-public ob getJob(@PathVariable String id) t
+public class Jobcontroller (
+  
+  private final Jobservice jobservice;
+
+  public JobController(Jobservice jobService {
+    this.jobservice = jobservice;
+  }
+
+  @PostMapping
+  public Job submitJob(@RequestBody Job job) {
+    job.setId(UUID. randomUUID().tostring());
+    return jobService.submitlob(job);
+  }
+
+  @GetMapping("/{id}")
+  public Job getJob(@PathVariable String id) {
+    return jobService.getJob(id);
+  }
