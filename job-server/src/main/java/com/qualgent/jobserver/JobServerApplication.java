41e044b1ea4cc415c173327d8bf7b0b1f537585d
SamSidari43 <samuelsidari@gmail.com>
2025-07-14 09:30:02 -0400

    Update JobServerApplication.java

--- job-server/src/main/java/com/qualgent/jobserver/JobServerApplication.java
+++ job-server/src/main/java/com/qualgent/jobserver/JobServerApplication.java
@@ -1,1 +1,7 @@
-
+package com qualgent. jobserver:
+import org. springfranework.boot SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication:
+©SpringBootApplication
+public class JobServerApplication
+public stati. void main(stringll angsyt
+SpringApplication.run(JobServerApplication.class.args)=
