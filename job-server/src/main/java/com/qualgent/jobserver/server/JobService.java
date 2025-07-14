38252ce22f1a194d6bdf96f0f3b3921145172fe7
SamSidari43 <samuelsidari@gmail.com>
2025-07-14 09:52:54 -0400

    Update JobService.java

--- job-server/src/main/java/com/qualgent/jobserver/server/JobService.java
+++ job-server/src/main/java/com/qualgent/jobserver/server/JobService.java
@@ -1,1 +1,14 @@
-
+package con.qualgent.jobserver.service;
+import com-qualgent.jobserver.model.Job;
+import org. springframework.stereotype.Service;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashhap;
+@Service
+• public class JobService {
+private Final Map<String, Job> jobStore = new ConcurrentHashMap<>();
+public Job submitJob(Job job) {
+jobStore.put (job.getId(), job);
+In a real app: enqueue job, group by app_version_id, assign to agent
+return job;
+public Job getJob(String id) t
+return jobstore get(id);
