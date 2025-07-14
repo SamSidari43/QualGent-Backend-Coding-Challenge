c5d65c0542e33b984b222301f73cf4cfe1e70cbd
SamSidari43 <samuelsidari@gmail.com>
2025-07-14 09:46:02 -0400

    Update Job.java

--- job-server/src/main/java/com/qualgent/jobserver/model/Job.java
+++ job-server/src/main/java/com/qualgent/jobserver/model/Job.java
@@ -1,1 +1,24 @@
-
+package com.qualgent. jobserver .model;
+public class Job {
+private String id; private String orgId;
+private String appVersionId;
+private String testPath;
+private String target;
+private String status = "QUEUED";
+I
+// getters and setters
+public String getId() ( return id; } public void setId(String id) ( this.id - id; }
+140
+117
+148
+public String getOrgId() { return orgid; }
+public void setOrgid(String orgid) { this.orgId = orgId; }
+public String getAppVersionId() { return appVersionid; }
+public void setAppVersionId(String appVersionId) { this.appVersionId = appVersionId; }
+1137
+public String getTestPath() { return testPath; }
+public void setTestPath(String testPath) { this.testPath = testPath; }
+public String getTarget() ( return target; }
+public void setTarget(String target) { this. target = target; }
+public String getStatus() { return status; }
+public void setStatus (String status) { this.status = status; }
